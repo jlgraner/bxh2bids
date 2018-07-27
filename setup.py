50f228d4d49df3e343fe4509044b1d0940979474
jlgraner <32852039+jlgraner@users.noreply.github.com>
2018-07-27 12:21:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 setup(
     name="bxh2bids",
+    description="Convert Duke BIAC imaging data to BIDS",
+    url="http://github.com/jlgraner/bxh2bids",
     install_requires=['xmltodict']
 )
