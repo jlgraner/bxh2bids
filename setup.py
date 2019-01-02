e15ee98cbc8bd49413911f871dfaf2d226deb7a1
jlgraner <32852039+jlgraner@users.noreply.github.com>
2019-01-02 13:31:23 -0500

    Set Version in setup.py
    Added a version in setup.py.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name="bxh2bids",
+    version="1.0",
     description="Convert Duke BIAC imaging data to BIDS",
     author="John Graner",
     author_email="john.graner@duke.edu",
