000682f5e875c563848c5911c3354ee90ef29096
jlgraner <32852039+jlgraner@users.noreply.github.com>
2019-01-02 14:57:21 -0500

    Include Package Data and Module Names in Setup.py
    Added package data information and module names to setup.py.

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
+pck_data = {
+            'bxh2bids': ['info_field_files/*.json',
+                         'Template_files/*.json',
+                         'Documentation/*.md'
+                         ]
+            }
+
 setup(
     name="bxh2bids",
     version="1.0",
@@ -7,5 +14,7 @@ setup(
     author="John Graner",
     author_email="john.graner@duke.edu",
     url="http://github.com/jlgraner/bxh2bids",
-    install_requires=['xmltodict']
+    py_modules=['bxh2bids', 'bxh_pick_fields'],
+    install_requires=['xmltodict'],
+    package_data=pck_data
 )
