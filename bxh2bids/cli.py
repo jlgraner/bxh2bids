ad305b4be4ab10a266065aaae5647a1ceb752129
John Graner <32852039+jlgraner@users.noreply.github.com>
2023-05-10 15:16:43 -0400

    Update cli.py
    Added more usage information to doc string.

--- bxh2bids/cli.py
+++ bxh2bids/cli.py
@@ -1,13 +1,49 @@
 r"""Convert MRI data from BIAC into BIDS format
 
 Notes:
-    If no proj-dir passed, uses $BIDS_DIR
 
-Examples:
+    A couple things must be in place before calling bxh2bids:
 
-    bxh2bids --biac-dirs 01011900_12345
+        1. A BIDS-format project directory must exist as a
+            destination for the images to be converted.
 
-    bxh2bids --proj-dir /path/bids_dir --biac-dirs 01011900_12345 01021900_56789
+        2. Functional task-related images must have .tsv events
+            already files created
+
+        3. A session information file must exist for each session
+            being converted (see below).
+
+            
+    SESSION INFO. FILES:
+
+        Session information files must be saved in the following location:
+            .../BIDS_DIR/code/bxh2bids_ses_info/
+
+        They must have the following name:
+            bxh2bids_YYYYMMDD_#####.json
+        YYYYMMDD_##### is the BIAC session
+
+        
+    PROJECT DIRECTORY:
+
+        If no project directory is passed when calling bxh2bids on the
+        command-line, it will try to use the value of the environment
+        variable $BIDS_DIR. If this has not been set, a project directory
+        must be passed using the --proj-dir option.
+    
+
+Example Usage:
+
+    Convert BIAC images from the 01011900_12345 session to BIDS, using
+    $BIDS_DIR as the target BIDS-format directory.
+
+        bxh2bids --biac-dirs 01011900_12345
+
+        
+    Convert BIAC images from sessions 01011900_12345 and 01021900_56789,
+    using /path/bids_dir as the target BIDS-format directory.
+
+        bxh2bids --proj-dir /path/bids_dir --biac-dirs 01011900_12345 01021900_56789
 
 """
 
@@ -70,9 +106,6 @@ def main():
     # Check proj_dir. If not passed, check for env variable.
     if proj_dir == 'None':
         try:
-            for element in os.environ:
-                value = os.environ[element]
-                print(f"{element}: {value}")
             proj_dir = os.environ["BIDS_DIR"]
         except KeyError:
             print(f"Global variable BIDS_DIR is not set and no proj_dir passed!")
